c4ba4763053b00583757eded4004efb9f6c4dd9a
Judith Nwagwu <Judith5nwagwu@gmail.com>
2021-01-02 21:10:10 -0800

    updating db.js

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -17,7 +17,7 @@ request.onsuccess = function (event) {
   };
   
   request.onerror = function (event) {
-    console.log("Woops! " + event.target.errorCode);
+    console.log("Uh Oh! " + event.target.errorCode);
   };
   
   function saveRecord(record) {
