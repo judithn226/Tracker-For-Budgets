69a0f62906e3c37afa43909e103a5be713b1e85c
Judith Nwagwu <Judith5nwagwu@gmail.com>
2021-01-03 13:37:17 -0800

    testing

--- server.js
+++ server.js
@@ -4,24 +4,33 @@ const logger = require("morgan");
 const db = require("./models");
 const app = express();
 const path = require("path");
+
 app.use(logger("dev"));
+
 app.get("/exercise", (req, res) => {
     res.sendFile(path.join(`${__dirname}/public/exercise.html`));
 });
+
 app.get("/stats", (req, res) => {
     res.sendFile(path.join(`${__dirname}/public/stats.html`));
 });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 require("./routes/routes.js")(app);
+
 const PORT = process.env.PORT || 3000;
+
 const DeprecationWarning = {
     useUnifiedTopology: true,
     useFindAndModify: false,
     useNewUrlParser: true,
+
+
 };
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://budgetTrack:1234@cluster0.i7njn.mongodb.net/BudgetTrack?retryWrites=true&w=majority", DeprecationWarning)
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://fitnesstracker:1234@cluster0.i7njn.mongodb.net/fitnesstrackerdb?retryWrites=true&w=majority", DeprecationWarning)
     .then(() => {
         app.listen(PORT, () => {
             console.log(`localhost:${PORT}`);
